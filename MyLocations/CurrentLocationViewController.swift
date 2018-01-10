ad7d0eb2fe8c64d90255203d107fdb998b0f200a
nguyenphuckhanh1991 <khanhnguyen.joy@gmail.com>
2018-01-10 10:12:39 +0700

    Init project

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by nguyen.phuc.khanh on 1/10/18.
+//  Copyright © 2018 khanh.nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    @IBAction func getLocation() {
+        // do nothing yet
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
